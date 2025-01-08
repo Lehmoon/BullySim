497eb3810cd1ff53841568ff55f888439087ba30
Lehmoon <elijahwyost1992@gmail.com>
2025-01-07 17:39:50 -0800

    New Map

--- Bully Simulator/Assets/Scripts/MovementScript.cs
+++ Bully Simulator/Assets/Scripts/MovementScript.cs
@@ -5,7 +5,16 @@ public class MovementScript : MonoBehaviour
 
     public CharacterController controller;
 
-    public float speed = 12f;
+    public float characterspeed = 12f;
+
+    public float gravity = -9.81f;
+   
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,13 +25,21 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        { velocity.y = -2f; }
+        
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(move * characterspeed * Time.deltaTime);
 
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
 
     }
 }
